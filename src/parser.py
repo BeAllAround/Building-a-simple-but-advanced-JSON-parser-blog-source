e002016c683220ef52004adb5ab7dbd1bc9f5cbf
Alex Milenkovic <38843286+BeAllAround@users.noreply.github.com>
2023-05-09 22:51:27 +0200

    Add files via upload

--- src/parser.py
+++ src/parser.py
@@ -2,6 +2,9 @@ class EndLine(Exception):
     pass
 
 class Char_stream:
+    pass
+
+class Char_stream:
 
     def __init__(self, source: str, c: int = 0):
         self.c = c # counter for our stream
@@ -28,28 +31,29 @@ class Char_stream:
     def is_over(self):
         return not self.c < len(self.source)
 
-    def set_cs(self, char_stream):
+    def set_cs(self, char_stream: Char_stream):
         self.source = char_stream.source
         self.c = char_stream.c
 
-class Token():
-    def __init__(self, tp, char): # TBD
+class Token:
+
+    def __init__(self, tp, char): # TODO
         self.tp = tp
         self.char = char
 
 # reuse Char_stream to extend Lexer and then Parser - adding new methods as we go
-class Lexer():
+class Lexer:
 
-    def __init__(self, expression):
-        self.char_stream = Char_stream(expression)
-        self.tokens = [] # List<Token>
+    def __init__(self, expression: str):
+        self.char_stream: Char_stream = Char_stream(expression)
+        self.tokens: list = [] # List<Token>
 
     def lex(self): # tokenize
         return self.tokens
 
 # use Parser with Lexer to generate AST
-class Parser(): 
+class Parser: 
 
-    def __init__(self, expression):
+    def __init__(self, expression: str):
         self.tokens = Lexer(expression).lex()
         self.ast = {}
